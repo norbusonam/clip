a1bdcc8c2bf807405bd819ba85529f523a1074c4
norbusonam <nsonam1822@gmail.com>
2023-11-04 22:03:36 -0700

    Initial Commit

---
+++ clip/clipApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipApp.swift
+//  clip
+//
+//  Created by Norbu Sonam on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct clipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
